5baf601670bc3b54c0627db3644cf76bd685f3a6
himanshusangwantech-source <himanshusangwantech@gmail.com>
2025-11-22 23:51:45 +0530

    props

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,14 +6,19 @@ import Greeting from './compnents/Greeting'
 import ProductInfo from "./compnents/ProductInfo";
 import UserList from "./compnents/UserList";
 import ProductList from "./compnents/ProductList";
+import Person from "./compnents/Person";
+import Products from "./compnents/Products";
+import Card from "./compnents/Card.jsx";
 function App() {
   return (
     <div>
       {/* <Header/>
       <MainContent/>
       <Footer/> */}
-      <UserList/>
-      <ProductList/>
+     <Card>
+      <h1>card data</h1>
+      <p>this card data</p>
+     </Card>
       
     </div>
   );
---
+++ vite-project/src/compnents/Products.jsx
@@ -0,0 +1,9 @@
+const Products = ({item,price}) =>{
+    return(
+        <div>
+            <h1>{item}</h1>
+            <p>{price}</p>
+        </div>
+    )
+}
+export default Products
